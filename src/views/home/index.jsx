c869f85a570a9f240bac43b5dc100f02cf9dc221
fzp <350556636@qq.com>
2019-11-14 11:23:35 +0800

    首页轮播图广告完成

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,23 +1,69 @@
 import React, { Component } from 'react'
 
-import { Button } from 'antd-mobile'
+import styles from './index.module.scss'
+
+import { Carousel } from 'antd-mobile'
+import { BASEURL } from '../../utils/url'
 
 export default class Index extends Component {
+  // 定义模型---把数据交给模型才能渲染
+  state = {
+    swipers: null, // 轮播图
+    imgHeight:212  // 轮播图图片高度
+  }
   componentDidMount() {
     // 获取轮播图数据
-    this.getSwiperDate()
+    this.getSwiperData()
   }
 
   // 获取轮播图数据的 方法
-  getSwiperDate = async () => {
-    const res = await this.http.get('/home/swiper')
-    console.log(res)
+  getSwiperData = async () => {
+    const result = await this.http.get('/home/swiper')
+    // console.log(result)
+    this.setState({
+      swipers: result.data.body
+    })
   }
+
+  // 渲染轮播图
+  renderSwiper = () => {
+    return (
+      <Carousel
+        autoplay
+        infinite
+        className={styles.swiper}
+      >
+        {this.state.swipers.map(item => (
+          <a
+            key={item.id}
+            href={"http://www.alipay.com"}
+            style={{
+              display: 'inline-block',
+              width: '100%',
+              height: this.state.imgHeight
+            }}
+          >
+            <img
+              src={`${BASEURL}${item.imgSrc}`}
+              alt=""
+              style={{ width: '100%', verticalAlign: 'top' }}
+              onLoad={() => {
+                // fire window resize event to change height
+                window.dispatchEvent(new Event('resize'))
+                this.setState({ imgHeight: 'auto' })
+              }}
+            />
+          </a>
+        ))}
+      </Carousel>
+    )
+  }
+
+  
   render() {
     return (
-      <div>
-        这里是首页--home
-        <Button type="warning">呵呵哈哈哈</Button>
+      <div className={styles.root}>
+        {this.state.swipers && this.renderSwiper()}
       </div>
     )
   }
